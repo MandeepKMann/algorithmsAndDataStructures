a3601ddcf86e6090af923a7ef480235386905784
MandeepMann <mandeep_mann30@hotmail.com>
2023-02-02 17:00:51 -0500

    learning some bigO stuff

---
+++ bigO/bigO.js
@@ -0,0 +1,127 @@
+/**
+ * TIME COMPLEXITY
+ * Big O Shorthands
+ * 1) Arithmetic operations are constant
+ * 2) Variable assignment is constant
+ * 3) Accessing elements in an array (by index) or orbject (by key) is constant
+ * 4) In a loop, the complexity is the length of the loop times the complexity of whatever happens inside of the loop
+ */
+
+
+// O(n) - Linear
+const addUpTo = (n) => {
+    let total = 0;
+    for (let i = 1; i <= n; i++) {
+        total += i;
+    }
+    return total;
+}
+
+// console.log(addUpTo(3))
+// console.log(addUpTo(100))
+
+// O(1) - Constant
+const addItUpTo = (n) => {
+    return n * (n + 1) / 2;
+}
+// console.log(addItUpTo(3))
+// console.log(addItUpTo(100))
+
+
+// BOTH algorithms above to the same thing, the second one being faster
+
+// Checking time performance of algorithm
+
+// var t1 = performance.now();
+// addUpTo(1000000000);
+// var t2 = performance.now();
+// console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds.`);
+
+// var t1 = performance.now();
+// addItUpTo(1000000000);
+// var t2 = performance.now();
+// console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds.`);
+
+// speed measurements with time MAY NOT be precise enough (Can differ due to machines, sometimes the same machine has different times etc. and SOOOO we use BIG O!!!!)
+
+// O(n)
+const countUpandDown = (n) => {
+    console.log("Going up!");
+    for (let i = 0; i < n; i++) {
+        console.log(i);
+    }
+    console.log("At the top!\nGoing down...");
+    for (let j = n - 1; j >= 0; j--) {
+        console.log(j);
+    }
+    console.log("Back down. Bye!");
+}
+// countUpandDown(10)
+
+// O(n^2)- Quadratic
+const printAllPairs = (n) => {
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            console.log (i, j);
+        }
+    }
+}
+// printAllPairs(5)
+
+//  O(n)
+const logAtLeastFive = (n) => {
+    for (let i = 1; i <= Math.max(5, n); i++) {
+        console.log(i);
+    }
+}
+
+// logAtLeastFive(1)
+// logAtLeastFive(30)
+
+//  O(1)
+const logAtMostFive = (n) => {
+    for (let i = 1; i <= Math.min(5, n); i++) {
+        console.log(i);
+    }
+}
+
+// logAtMostFive(2)
+// logAtMostFive(10)
+
+/**
+ * O(log n)
+ * 
+ */
+
+
+
+
+/*********************************************************************************************************************************************************************************
+ * SPACE COMPLEXITY
+ * 
+ * Rules of Thumb:
+ * 1) Most primites (booleans, numbers, undefines, null) are constant space
+ * 2) String require 0(n) space (where n is the string length)
+ * 3) Reference types are generallt O(n), where n is the lenght (for arrays) or the number of keys (for objects)
+ * 
+ */
+
+const sum = (arr) => {
+    let total = 0;
+    for (let i = 0; i < arr.length; i++) {
+        total += arr[i];
+    }
+    return total;
+}
+// The above example only has two variables (total and i), it does not matter what the array is, meaning that they take up a constant space, O(1) space because that always remains the same no matter the size of the input
+
+const double = (arr) => {
+    let newArr = [];
+    for (let i = 0; i < arr.lenght; i++) {
+        newArr.push(2 * arr[i]);
+    }
+    return newArr;
+}
+// The above example, the newArr and I takes up 1 constant space each, but THEN the creation of the new array is dependent on the size of the size of the arr variables THEREFORE O(n) space!
+
+
