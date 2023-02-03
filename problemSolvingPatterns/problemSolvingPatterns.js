67ba0e72646f545f5891147b910b89ed5011a5e9
MandeepMann <mandeep_mann30@hotmail.com>
2023-02-03 13:24:54 -0500

    problem solving notes

---
+++ problemSolvingPatterns/problemSolvingPatterns.js
@@ -0,0 +1,66 @@
+/**
+ * WHAT EVEN IS AN ALGORITHM?
+ * just a process or set of steps to accomplish a certain task (:
+ */
+
+/**
+ * How do you improve?
+ * 1) DEVISE a plan for solving problems
+ * 2) MASTER common problem solving patterns
+ */
+
+/**
+ * THE PLANS:
+ * 1) Understand the problem
+ * 2) Explore concrete exmaples
+ * 3) Break it down
+ * 4) Solve/Simplify
+ * 5) Look back and refactor
+ */
+
+/**
+ * UNDERSTAND THE PROBLEM
+ * 1) Can i restate the problem in my own words?
+ * 2) what are the inputs that go into the problem?
+ * 3) what are the outputs that shuold come from the solution to the problem?
+ * 4) Can the outputs be determined from the inputs? In other words, do I have enough information to solve the problem? (You may not be able to answer this question until you set about solving the problem. That's ok; it's still worth considering the question at this early stage)
+ * 5) How should I label the important pieces of data that are a part of the problem?
+ */
+
+/**
+ * CONCRETE EXAMPLES
+ * 1) Start with simple examples
+ * 2) progress to more complex examples
+ * 3) explore examples with empty inputs
+ * 4) explore examples with invalid inputs
+ */
+
+/**
+ * BREAK IT DOWN
+ * Explicitly write out the steps you need to take
+ * This forces you to think about the code you'll write before you write it, and helps you catch any lingering conceptual issues or misunderstandings befre you dive in and have to worry about details 
+ */
+
+
+/**
+ * SOLVE OR SIMPLIFY
+ * Solved the problem, and if you can't, solve a simpler problem
+ * Instead of making zero progress at all, solve smaller parts of the problem
+ * You may gain insight into the more dofficult parts of the problem
+ */
+
+/**
+ * LOOK BACK AND REFACTOR
+ * Talk about what you like and don't like about it
+ * how easy is it to understand?
+ * ask these types of questions out loud "I think maybe there's a better way of doing this, but I'm not confident I would Google if there was a faster way of doing this, that sort of thing."
+ * Refactoring Questions to ask yourself:
+ * 1) Can you check the results?
+ * 2) Can you derive the results differently? There's rarely one solution to a problem. Is there another way that seems viable?
+ * 3) Can you understand it at a glance? How intuitive is your solution? Does it make sense?
+ * 4) Can you use the result or method for some other problem?
+ * 5) Can you improve the performance of your solution?
+ * 6) Can you think of other ways to refactor?
+ * 7) How have other people solved this problem?
+ */
+
